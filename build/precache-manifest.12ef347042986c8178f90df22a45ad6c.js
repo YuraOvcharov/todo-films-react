b0c547eede5dd5b82501aefe3a07534c1553a0a6
YuraOvcharov <yuraovcharov13@gmail.com>
2018-12-24 13:33:41 +0300

    change gitignore

---
+++ build/precache-manifest.12ef347042986c8178f90df22a45ad6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "682966c910b58ab9cc81",
+    "url": "/static/js/main.682966c9.chunk.js"
+  },
+  {
+    "revision": "20fb8069bb70a73eba68",
+    "url": "/static/js/1.20fb8069.chunk.js"
+  },
+  {
+    "revision": "682966c910b58ab9cc81",
+    "url": "/static/css/main.2e6e2c90.chunk.css"
+  },
+  {
+    "revision": "20fb8069bb70a73eba68",
+    "url": "/static/css/1.1e4315b3.chunk.css"
+  },
+  {
+    "revision": "adec35a02aba4b4de4491bc16da11ca9",
+    "url": "/index.html"
+  }
+];
